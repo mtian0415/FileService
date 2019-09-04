19c702bb77e0e64e78b40ff87981a978c2b4a66c
mtian0415 <tmwj1001@gmail.com>
2019-09-04 00:34:28 -0700

    Add FileService project

---
+++ FileService/FileService/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace FileService.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
